ddc620791165f0dd6e22b8d001071f2acc2ee98c
Youssef Negm <usf.ngm@gmail.com>
2018-06-22 03:13:46 +0200

    Hello

---
+++ modules/users/manage/ctrl_manage_user.js
@@ -0,0 +1,72 @@
+angular
+    .module("digAPP")
+    .controller('manageUserCtrl', function ($scope, $rootScope, $state, $stateParams, $window, $timeout, $http) {
+
+        var user;
+        $scope.uid = $stateParams.userID;
+        console.log('EDIT ' + $scope.uid);
+        $('#manageSingleUserContainer').hide();
+        $rootScope.isLoading = true;
+
+        var docRef = db
+            .collection("users")
+            .doc($scope.uid);
+
+        docRef
+            .get()
+            .then(function (doc) {
+                if (doc.exists) {
+                    user = doc.data();
+                    $scope.userName = user.name;
+                    $scope.userEmail = user.email;
+                    $scope.userMobile = user.mobile;
+                    $scope.userCountry = user.country;
+                    $scope.userCity = user.city;
+
+                    $rootScope.isLoading = false;
+                    $rootScope.$digest();
+                    $('#manageSingleUserContainer').show();
+                } else {
+                    $rootScope.isLoading = false;
+                    $rootScope.$digest();
+                    $state.go('viewAddUsers');
+                }
+            })
+            .catch(function (error) {
+                console.log("Error getting document:", error);
+                $rootScope.isLoading = false;
+                $rootScope.$digest();
+                $('#manageSingleUserContainer').show();
+            });
+
+        $scope.updateUser = () => {
+
+            $rootScope.isLoading = true;
+
+            user['email'] = $scope.userEmail;
+            user['name'] = $scope.userName;
+            user['mobile'] = $scope.userMobile;
+            user['city'] = $scope.userCity;
+            user['country'] = $scope.userCountry;
+
+            var docRef = db
+                .collection("users")
+                .doc($scope.uid);
+
+            docRef
+                .set(user)
+                .then((doc) => {
+                    $rootScope.isLoading = false;
+                    $rootScope.$digest();
+                    $state.go('viewAddUsers');
+                    toastr.success("User Updated");
+                })
+                .catch((error) => {
+                    $rootScope.isLoading = false;
+                    $rootScope.$digest();
+                    toastr.error("Error");
+                });
+
+        }
+
+    });
